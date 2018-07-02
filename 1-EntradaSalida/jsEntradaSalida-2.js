28e8010ac2ee42a8ad8786ad1702fb03bda6bd4f
Nicolas Fernandez <40763650+NicolasFernandez8@users.noreply.github.com>
2018-07-02 12:09:01 -0300

    Ejercicios 1 2 3
    Los primeros 3 Ejercicios

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,16 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
+	var numeroA = 2;
+	var numeroB = 4;
 	
+Alert (numeroA+numeroB);
+
+
+
+
+
+
 }
 
