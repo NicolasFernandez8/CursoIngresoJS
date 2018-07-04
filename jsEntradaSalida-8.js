ff544db3306c8ec24a7c91ebc3ee57c15b784638
Nicolas Fernandez <40763650+NicolasFernandez8@users.noreply.github.com>
2018-07-04 11:22:55 -0300

    Entrada y Salida

---
+++ jsEntradaSalida-8.js
@@ -0,0 +1,13 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resto entre el dividendo y el divisor.
+ej.: "El resto es 0 ."*/
+function SacarResto()
+{
+	var numeroUno;
+	var numeroDos;
+	numeroUno=parseInt(document.getElementById('numeroDividendo').value);
+	numeroDos=parseInt(document.getElementById('numeroDivisor').value);
+	resultado=numeroUno%numeroDos;
+	alert(resultado);
+}
